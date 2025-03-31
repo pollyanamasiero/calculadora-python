a5960f56262c9dd70aa931892926b157c36f43ad
pollyanamasiero <pollyanamasiero@hotmail.com>
2025-03-30 21:59:06 -0300

    correção da conversão de string para inteiro

--- nome_idade.py
+++ nome_idade.py
@@ -1,4 +1,9 @@
-nome = input('Olá! Qual é o seu nome? ')
-idade = input('Qual sua idade? ')
+# Criar um programa que recebe nome e idade e retorna uma mensagem formatada.
 
-print(f'Olá, {nome}! Você tem {idade} anos!')
+nome = input('Olá! Qual é o seu nome? ')
+anoNascimento = int(input('Em que ano você nasceu? ')) #Converte string em inteiro
+ano = 2025
+idade = ano - anoNascimento
+
+print(f'Olá, {nome}! Você nasceu em {anoNascimento}!')
+print(f'Esse ano você fez/faz {idade}!')
