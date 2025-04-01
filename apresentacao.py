0f5c6829f0c40560dc0e4ba1a4be6201510648f6
pollyanamasiero <pollyanamasiero@hotmail.com>
2025-03-31 21:01:38 -0300

    adição do exercício apresentação

---
+++ apresentacao.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+cidade = input('Digite sua cidade: ')
+
+print(f'Olá! Seu nome é {nome}, você tem {idade} anos e você mora em {cidade}.')
