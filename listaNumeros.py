e1d0e01b2d4c5c76c68980e6a85992d846bca313
pollyanamasiero <pollyanamasiero@hotmail.com>
2025-03-31 21:12:29 -0300

    adição do exercício listaNumeros

---
+++ listaNumeros.py
@@ -0,0 +1,8 @@
+numeros = [10, 20, 30, 40, 50]
+
+soma = 0
+for numero in numeros: # PARA cada NÚMERO dentro da lista NÚMEROS
+    soma += numero 
+    
+media = soma / len(numeros)
+print(f'A média dos números é: {media}.')
